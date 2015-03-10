1d57f860f43a4dff62d87dbf31205eeab269e8ae
Michelle Tan <michelletanjm@gmail.com>
2015-03-10 12:05:05 +0800

    Initial commit

---
+++ Collect/Collect/CLTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CLTableViewCell.swift
+//  Collect
+//
+//  Created by Michelle Tan on 10/3/15.
+//  Copyright (c) 2015 Mola Mola Studios. All rights reserved.
+//
+
+import UIKit
+
+class CLTableViewCell: UITableViewCell {
+    
+}
---
+++ Collect/Collect/CollectForm.swift
@@ -0,0 +1,19 @@
+//
+//  CollectForm.swift
+//  Collect
+//
+//  Created by Michelle Tan on 10/3/15.
+//  Copyright (c) 2015 Mola Mola Studios. All rights reserved.
+//
+
+import UIKit
+
+class CollectForm: UITableViewController {
+    enum CellType {
+        case TextOneLine
+        case TextTwoLines
+        case TextMultipleLines
+        case Media
+        case ColorPicker
+    }
+}
